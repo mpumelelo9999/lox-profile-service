e8b7d32bf88f614aa4377925a13b9c2b91752f7d
Prateek Kishore Karnad <trainer.it.ssk@gmail.com>
2024-11-27 21:37:33 +0530

    Initial commit

---
+++ src/main/java/com/secor/loxprofileservice/LoxProfileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.secor.loxprofileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoxProfileServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoxProfileServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/secor/loxprofileservice/LoxProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secor.loxprofileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoxProfileServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
